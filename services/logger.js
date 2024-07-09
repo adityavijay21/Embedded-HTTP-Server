d882d54a2fec83cb704cae5d3569f4b9ee56bdff
Aditya Vijay <kiriotheo@gmail.com>
2024-07-10 01:17:29 +0530

    Create logger.js

---
+++ services/logger.js
@@ -0,0 +1,20 @@
+const winston = require('winston');
+const config = require('../config');
+
+const logger = winston.createLogger({
+  level: config.logLevel,
+  format: winston.format.json(),
+  defaultMeta: { service: 'user-service' },
+  transports: [
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'combined.log' }),
+  ],
+});
+
+if (process.env.NODE_ENV !== 'production') {
+  logger.add(new winston.transports.Console({
+    format: winston.format.simple(),
+  }));
+}
+
+module.exports = logger;
