d546a919540e18d596f237c763e85e3e3044a3a9
Aditya Vijay <kiriotheo@gmail.com>
2024-07-10 01:16:40 +0530

    Create user.js

---
+++ routes/user.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const auth = require('../middleware/auth');
+const User = require('../models/user');
+
+const router = express.Router();
+
+router.get('/me', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+module.exports = router;
